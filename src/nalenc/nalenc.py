b2b07074c7f986358491aad7bede20965fddf271
AsfhtgkDavid <daika@i.ua>
2025-04-19 17:13:32 +0300

    feat: add vectorize for large data

--- src/nalenc/nalenc.py
+++ src/nalenc/nalenc.py
@@ -81,7 +81,7 @@ class NALEnc:
                                                       self.__prepared_passwds[i - 1])
 
     def __finish_message(self, msg: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
-        additional_len = 2046 - (len(msg) % 2046) + ((len(msg) // 2048) * 2) % 2046
+        additional_len = (2048 - (len(msg) + 2) % 2048) % 2048
         if additional_len != 2046 or len(msg) % 2048 == 0:
             res = np.empty(len(msg) + additional_len + 2, np.uint8)
             res[2:len(msg) + 2] = msg
